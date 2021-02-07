1927d2de0ed791ec2a82cf4ad721e9da936fbb3a
hojune-choi <hojune.choi.me@gmail.com>
2021-02-07 09:22:16 +0900

    v0.1.2 - 템플릿, 모델 생성

--- signoff/models.py
+++ signoff/models.py
@@ -5,3 +5,4 @@ class Signoff(models.Model):
     name = models.CharField(max_length=30)
     qnt = models.IntegerField()
     unit = models.IntegerField()
+    request_date = models.DateField(auto_now_add=True)
--- signoff/views.py
+++ signoff/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Signoff
 # Create your views here.
 def index(request):
-    pass
+    requests = Signoff.objects.order_by('id').values()
+    context = {
+        'requests': requests
+    }
+    return render(request, 'request_list.html', context)
